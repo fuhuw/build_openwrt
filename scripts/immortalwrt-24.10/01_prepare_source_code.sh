3a1643e9d77b1fa3613bf6d8f422cb8ef248cede
fuhuw <fuhuwww@gmail.com>
2025-10-22 09:32:44 +0800

    Update 01_prepare_source_code.sh

--- scripts/immortalwrt-24.10/01_prepare_source_code.sh
+++ scripts/immortalwrt-24.10/01_prepare_source_code.sh
@@ -7,7 +7,7 @@ clone_repo $immortalwrt_repo ${1} openwrt &
 clone_repo $openwrt_pkg_repo master openwrt_pkg_ma &
 clone_repo $node_prebuilt_repo packages-24.10 node &
 clone_repo $openwrt_apps_repo main openwrt-apps &
-clone_repo $amlogic_repo main amlogic &
+
 # 等待所有后台任务完成
 rm -rf feeds/luci/applications/luci-app-momo
 rm -rf feeds/luci/applications/luci-app-alist
@@ -17,49 +17,11 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwal
 git clone https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git clone https://github.com/sbwml/luci-app-openlist2 package/luci-app-openlist2
 #git_sparse_clone main https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
-rm -rf package/small-package/luci-app-openvpn-server
-rm -rf package/small-package/openvpn-easy-rsa-whisky
-rm -rf package/small-package/luci-app-wrtbwmon
-rm -rf package/small-package/wrtbwmon
-rm -rf package/small-package/luci-app-koolproxy
-rm -rf package/small-package/luci-app-godproxy
-rm -rf package/small-package/luci-app-passwall2
-#rm -rf package/small-package/luci-theme-argon*
-rm -rf package/small-package/luci-app-amlogic
-rm -rf package/small-package/cdnspeedtest
-rm -rf package/small-package/luci-app-unblockneteasemusic
-rm -rf package/small-package/upx-static
-rm -rf package/small-package/upx
-rm -rf package/small-package/firewall*
-rm -rf package/small-package/opkg
-rm -rf package/small-package/base-files
-rm -rf package/small-package/luci-app-bandwidthd
-rm -rf package/small-package/luci-app-bypass
-rm -rf package/small-package/luci-app-dogcom
-rm -rf package/small-package/luci-app-gowebdav
-rm -rf package/small-package/luci-app-nginx-pingos
-rm -rf package/small-package/luci-app-ssr-plus
-rm -rf package/small-package/luci-app-onliner
-rm -rf package/small-package/natflow
-rm -rf package/small-package/luci-app-cloudflarespeedtest
-#rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
-rm -rf package/feeds/packages/aliyundrive-webdav
-rm -rf feeds/packages/multimedia/aliyundrive-webdav
-rm -rf package/feeds/packages/perl-xml-parser
-rm -rf package/feeds/packages/xfsprogs
-#rm -rf package/feeds/packages/alist
-#rm -rf feeds/packages/net/alist
-rm -rf feeds/luci/applications/luci-app-openlist
-rm -rf package/small-package/luci-app-openlist
-rm -rf feeds/luci/applications/luci-app-openlist2
-rm -rf package/small-package/luci-app-openlist2
-rm -rf package/small-package/alist
-rm -rf package/small-package/openlist2
-rm -rf feeds/luci/applications/openlist2
+git clone --depth=1 https://github.com/nikkinikki-org/OpenWrt-momo package/momo
 
 
 wait
-sed -i 's/immortalWrt/redmek40/g' package/base-files/files/bin/config_generate
+sed -i 's/ImmortalWrt/redmek40/g' package/base-files/files/bin/config_generate
 # 设置默认密码为 password
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' openwrt/package/base-files/files/etc/shadow
 # 修改默认 IP 为 192.168.1.99
